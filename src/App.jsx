f9bf3fa454807b6a93bbdb6a4ca20f00932ac710
MahinAshraful <mahinashraful08@gmail.com>
2024-07-11 14:36:10 -0400

    using map

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const [value, setValue] = useState(null)
   const [operand, setOperand] = useState('')
 
+
+
   const handleOperation = (operation) =>{
       setValue(count)
       setMethod(operation)
@@ -39,28 +41,39 @@ function App() {
     setOperand("")
   } 
 
+  const nums = [1,2,3,4,5,6,7,8,9,0] 
+  const ops = ["add", "sub", "mul", "div"]
+  // 1,2,3,'add'
+  // 4,5,6,'sub'
+  // 7,8,9, 'mul'
+  // C,0,=,'div'
+
 
   return (
     <div className="App">
       <div className="calculator">
         <div className="display">{value}{operand}{count}</div>
         <div className="buttons">
-          <button onClick={() => setCount(count + "1")}>1</button>
-          <button onClick={() => setCount(count + "2")}>2</button>
-          <button onClick={() => setCount(count + "3")}>3</button>
+          {
+            nums.map((value, index) => {
+              
+              return (
+                <button key={index} onClick={() => setCount(count + String(value))}>
+                  {value}
+                </button>
+              );
+            })
+          }
+
+
           <button onClick={() => handleOperation("add")}>+</button>
-          <button onClick={() => setCount(count + "4")}>4</button>
-          <button onClick={() => setCount(count + "5")}>5</button>
-          <button onClick={() => setCount(count + "6")}>6</button>
           <button onClick={() => handleOperation("sub")}>-</button>
-          <button onClick={() => setCount(count + "7")}>7</button>
-          <button onClick={() => setCount(count + "8")}>8</button>
-          <button onClick={() => setCount(count + "9")}>9</button>
           <button onClick={() => handleOperation("mul")}>*</button>
-          <button onClick={() => setCount('')}>C</button>
-          <button onClick={() => setCount(count + "0")}>0</button>
-          <button onClick={equals}>=</button>
           <button onClick={() => handleOperation("div")}>/</button>
+
+
+          <button onClick={() => setCount('')}>C</button>
+          <button onClick={equals}>=</button>
         </div>
       </div>
     </div>
